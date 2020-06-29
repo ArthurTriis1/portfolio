5515cb973d60aea3bb106523ad638bfc7fa7e991
aluno <arthurfelandrade@gmail.com>
2020-06-29 14:09:40 -0300

    Dinamic colab page

--- src/Pages/Colab/index.js
+++ src/Pages/Colab/index.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import InfoContainer from '../../Components/InfoContainer';
 import ColabSpan from '../../Components/ColabSpan';
 
 import './Colab.css';
-import hackathon from '../../assets/temp/hackathon.png'
-import grendes from '../../assets/temp/grendes.jpeg'
-import developer from '../../assets/temp/developer.jpg'
+import apiPortfolio from '../../services/apiPortfolio';
 
 function Colab() {
+
+    const [ colabs, setColabs ] = useState([])
+
+    useEffect(() => {
+        (async () =>{
+            const { data } = await apiPortfolio.get("colabs");
+            setColabs(data);
+        })()
+    }, [])
+
     return (
         <section className="containerColab">
             <InfoContainer
@@ -15,24 +23,18 @@ function Colab() {
                 text="Aqui são algumas participações em eventos, lives e trabalhos por aí. Espero que goste! (Clique e saiba mais)"
             />
             <div className="colabsWrap">
-                <ColabSpan
-                    href="http://google.com"
-                    image={hackathon}
-                    title="Hackathon CCR"
-                    type="Evento"
-                />
-
-                <ColabSpan
-                    image={grendes}
-                    title="Facebook Developer Recife"
-                    type="Live"
-                />
-
-                <ColabSpan
-                    image={developer}
-                    title="Grupó GRENDES"
-                    type="Pesquisa"
-                />
+                {
+                    colabs &&
+                    colabs.map(colab => (
+                        <ColabSpan
+                            key={colab.id}
+                            href={colab.link}
+                            image={`${process.env.REACT_APP_BACKEND_URL}${colab.image.formats.small.url}`}
+                            title={colab.title}
+                            type={colab.description}
+                        />
+                    ))
+                }
             </div>
 
         </section>
